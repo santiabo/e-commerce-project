73ffdc9f18a454030c7b90643a643cff872ad14b
Chusto <38694567+Chusto@users.noreply.github.com>
2020-12-08 20:26:07 -0300

    S3

--- api/index.js
+++ api/index.js
@@ -23,6 +23,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(5432, () => {
-    console.log('%s listening at 5432'); // eslint-disable-line no-console
+    console.log('is listening at 5432'); // eslint-disable-line no-console
   });
 });
