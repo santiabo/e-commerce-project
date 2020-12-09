9be52b66c16a46a9e1c94b302e848246f4d50ffd
Ivanovicc <ivanacosta456@gmail.com>
2020-12-09 19:36:13 -0300

    S26/27 Rutas para modificar y eliminar

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,37 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+// ------- Update Route -------
+server.put('/products/:id', (req, res, next) => {
+	const { id } = req.params;
+	const { name, description } = req.body;
+
+	Product.update({
+		name,
+		description
+	}, {
+		where: { id }
+	})
+		.then(data => {
+			if (!data) {
+				return res.status(400).send({ error: 'Product Not Found' });
+			}
+			return res.status(200).send(data);
+		})
+		.catch(next);
+})
+
+// ------- Delete Route -------
+server.delete('/products/:id', (req, res, next) => {
+	const { id } = req.params;
+
+	Product.destroy({
+		where: { id }
+	})
+		.then(() => {
+			return res.status(200).send('Product Delete');
+		})
+		.catch(next);
+})
+
 module.exports = server;
