2b9f39ff4cb32d53252c46c28837756bb7533da3
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2020-12-09 15:48:53 -0300

    S19/20 Rutas para eliminar y modificar

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,45 @@
 const server = require('express').Router();
-const { Product } = require('../db.js');
+const { Product, Category } = require('../db.js');
 
 server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+    Product.findAll()
+        .then(products => {
+            res.send(products);
+        })
+        .catch(next);
 });
+
+//DELETE
+server.delete('/category/:id', (req, res) => {
+    const { id } = req.params;
+    Category.destroy({
+        where: { id }
+    })
+        .then(() => {
+            res.send('Category Deleted')
+        })
+        .catch(err => {
+            res.send('Error:', err)
+        })
+});
+
+//PUT
+server.put('/category/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    Category.update({
+        name,
+        description
+    }, {
+        where: { id }
+    })
+        .then(() => {
+            return res.status(200).send('Updated')
+        })
+        .catch(err => {
+            res.send('Error:', err)
+        })
+})
+
 
 module.exports = server;
