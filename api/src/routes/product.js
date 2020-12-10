27780065b22dcd2bee0934b67af9ab7094fce695
gonzaloaguilarm <gonzaloaguilarm@gmail.com>
2020-12-10 11:55:16 -0300

    Category Route Created & Fixes

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,15 +1,69 @@
 const server = require('express').Router();
 const { Product, Category } = require('../db.js');
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
+const {
+	getAll,
+	createOne,
+	getOne,
+	editOne,
+	deleteOne
+} = require('../controllers/products');
+
+// ------- Product Route -------
+server.route('/').get((req, res) => {
+		getAll()
+			.then(products => res.status(201).json(products))
+			.catch(err => res.status(400).json(err));
+	}).post((req, res) => {
+		const {
+			name,
+			description,
+			price,
+			stock,
+			images
+		} = req.body;
+		createOne(name, description, price, stock, images)
+			.then(product => res.status(201).json(product))
+			.catch(err => res.status(400).json(err));
+	})
+
+// ------- Update Product Route -------
+server.put('/:id', (req, res, next) => {
+	const { id } = req.params;
+	const { name, description, price, stock, images } = req.body;
+
+	Product.update({
+		name,
+		description,
+		price,
+		stock,
+		images
+	}, {
+		where: { id }
+	})
+		.then(data => {
+			if (!data) {
+				return res.status(400).send({ error: 'Product Not Found' });
+			}
+			return res.status(200).send(data);
 		})
 		.catch(next);
-});
+})
 
-//POST /products/:idProducto/category/:idCategoria Uni la categoria al producto.
+// ------- Delete Product Route -------
+server.delete('/:id', (req, res, next) => {
+	const { id } = req.params;
+
+	Product.destroy({
+		where: { id }
+	})
+		.then(() => {
+			return res.status(200).send('Product Deleted');
+		})
+		.catch(next);
+})
+ 
+// ------- Add Product to Category Route -------
 
 server.post('/:idProducto/category/:idCategoria', (req, res) => {
 	const idProducto = req.params.idProducto;
@@ -30,9 +84,8 @@ server.post('/:idProducto/category/:idCategoria', (req, res) => {
 		})
 })
 
+// ------- Delete Category from Product Route -------
 
-
-//DELETE /products/:idProducto/category/:idCategoria(prueba)
 server.delete('/:idProducto/category/:idCategoria', (req, res) => {
 	const idProducto = req.params.idProducto;
 	const idCategoria = req.params.idCategoria;
@@ -52,8 +105,21 @@ server.delete('/:idProducto/category/:idCategoria', (req, res) => {
 		})
 })
 
+// ------- Add Category Route -------
+server.post('/category/', (req, res, next) => {
+	const { name,description } = req.body;
 
-//DELETE
+	Category.create({
+      name: name,
+      description:description    
+	})
+	.then(()=>{
+		res.send('Category created')
+	})
+	.catch(next)
+})
+
+// ------- Delete Category Route -------
 server.delete('/category/:id', (req, res) => {
 	const { id } = req.params;
 	Category.destroy({
@@ -72,7 +138,7 @@ server.delete('/category/:id', (req, res) => {
 		})
 });
 
-//PUT
+// ------- Update Category Route -------
 server.put('/category/:id', (req, res) => {
 	const { id } = req.params;
 	const { name, description } = req.body;
