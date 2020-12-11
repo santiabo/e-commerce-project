0a68864103a48c64dbfb2a43450d03c6ba009d63
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-12-11 08:22:46 -0300

    Added GET products by category name.

--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -2,113 +2,113 @@ const { Product } = require('../db');
 
 // Este método se encargará de obtener todos los productos
 const getAll = () => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-        Product.findAll({})
-            .then((products) => {
-                if (products.length === 0) {
-                    return reject({
-                        error: {
-                            name: "ApiFindError",
-                            type: "Products Error",
-                            errors: [
-                                {
-                                    message: "there are no products in the database",
-                                    type: "not found",
-                                    value: null,
-                                },
-                            ],
-                        },
-                    });
-                }
+    Product.findAll({})
+      .then((products) => {
+        if (products.length === 0) {
+          return reject({
+            error: {
+              name: "ApiFindError",
+              type: "Products Error",
+              errors: [
+                {
+                  message: "there are no products in the database",
+                  type: "not found",
+                  value: null,
+                },
+              ],
+            },
+          });
+        }
 
-                resolve(products);
-            })
-            .catch((err) => reject({ error: err }));
-    });
+        resolve(products);
+      })
+      .catch((err) => reject({ error: err }));
+  });
 };
 
 
 // Crea un producto recibiendo como parámetros: nombre, descripción, precio, stock e imagenes
 const createOne = (name, description, price, stock, images) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-        Product.create({ name, description, price, stock, images })
-            .then(product => {
-                if (stock) {
-                    product.stock = stock;
-                    product.save();
-                }
+    Product.create({ name, description, price, stock, images })
+      .then(product => {
+        if (stock) {
+          product.stock = stock;
+          product.save();
+        }
 
-                resolve(product);
-            })
-            .catch(err => reject({ error: err }));
-    })
-}
+        resolve(product);
+      })
+      .catch(err => reject({ error: err }));
+  });
+};
 
 // Obtiene un único producto por su id
 const getOne = (id) => {
-    return new Promise((resolve, reject) => {
-        Product.findOne({
-            where: { id },
-            include: [Category, Image, Review]
-        })
-            .then(product => {
-                if (!product) {
-                    return reject({
-                        error: {
-                            name: 'ApiFindError',
-                            type: 'Products Error',
-                            errors: [
-                                {
-                                    message: 'product does not exist in the database',
-                                    type: 'not found',
-                                    value: null,
-                                },
-                            ],
-                        },
-                    });
-                }
-                resolve(product);
-            })
-            .catch(err => reject({error: err}));
-    });
+  return new Promise((resolve, reject) => {
+    Product.findOne({
+      where: { id },
+      include: [Category, Image, Review]
+    })
+      .then(product => {
+        if (!product) {
+          return reject({
+            error: {
+              name: 'ApiFindError',
+              type: 'Products Error',
+              errors: [
+                {
+                  message: 'product does not exist in the database',
+                  type: 'not found',
+                  value: null,
+                },
+              ],
+            },
+          });
+        }
+        resolve(product);
+      })
+      .catch(err => reject({ error: err }));
+  });
 };
 
 // Dado el Id de un producto, lo encuentra y lo modifica
 const editOne = (id, name, description, price, stock, imageUrl) => {
-    return new Promise((resolve, reject) => {
-      getOne(id)
-        .then((product) => {
-          
-          if (name) product.name = name.toLowerCase();
-          if (description) product.description = description;
-          if (price) product.price = price;
-          if (stock) product.stock = stock;
+  return new Promise((resolve, reject) => {
+    getOne(id)
+      .then((product) => {
 
-          return product.save();
-        })
-        .then((product) => resolve(product))
-        .catch((err) => reject({ error: err }));
-    });
-  };
+        if (name) product.name = name.toLowerCase();
+        if (description) product.description = description;
+        if (price) product.price = price;
+        if (stock) product.stock = stock;
 
-  const deleteOne = (id) => {
-      return new Promise((resolve, reject) => {
-          getOne(id)
-            .then(product => {
-                product.destroy();
+        return product.save();
+      })
+      .then((product) => resolve(product))
+      .catch((err) => reject({ error: err }));
+  });
+};
 
-                resolve({ description: 'successfully remove object' })
-            })
-            .catch(err => reject (err));
-      });
-  }
+const deleteOne = (id) => {
+  return new Promise((resolve, reject) => {
+    getOne(id)
+      .then(product => {
+        product.destroy();
+
+        resolve({ description: 'successfully remove object' });
+      })
+      .catch(err => reject(err));
+  });
+};
 
 module.exports = {
-    getAll,
-    createOne,
-    getOne,
-    editOne,
-    deleteOne
-}
+  getAll,
+  createOne,
+  getOne,
+  editOne,
+  deleteOne
+};
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -2,165 +2,183 @@ const server = require('express').Router();
 const { Product, Category } = require('../db.js');
 
 const {
-	getAll,
-	createOne,
-	getOne,
-	editOne,
-	deleteOne
+  getAll,
+  createOne,
+  getOne,
+  editOne,
+  deleteOne
 } = require('../controllers/products');
 
 // ------- Product Route -------
 server.route('/').get((req, res) => {
-		getAll()
-			.then(products => res.status(201).json(products))
-			.catch(err => res.status(400).json(err));
-	}).post((req, res) => {
-		const {
-			name,
-			description,
-			price,
-			stock,
-			images
-		} = req.body;
-		createOne(name, description, price, stock, images)
-			.then(product => res.status(201).json(product))
-			.catch(err => res.status(400).json(err));
-	})
+  getAll()
+    .then(products => res.status(201).json(products))
+    .catch(err => res.status(400).json(err));
+}).post((req, res) => {
+  const {
+    name,
+    description,
+    price,
+    stock,
+    images
+  } = req.body;
+  createOne(name, description, price, stock, images)
+    .then(product => res.status(201).json(product))
+    .catch(err => res.status(400).json(err));
+});
 
 // ------- Update Product Route -------
 server.put('/:id', (req, res, next) => {
-	const { id } = req.params;
-	const { name, description, price, stock, images } = req.body;
+  const { id } = req.params;
+  const { name, description, price, stock, images } = req.body;
 
-	Product.update({
-		name,
-		description,
-		price,
-		stock,
-		images
-	}, {
-		where: { id }
-	})
-		.then(data => {
-			if (!data) {
-				return res.status(400).send({ error: 'Product Not Found' });
-			}
-			return res.status(200).send(data);
-		})
-		.catch(next);
-})
+  Product.update({
+    name,
+    description,
+    price,
+    stock,
+    images
+  }, {
+    where: { id }
+  })
+    .then(data => {
+      if (!data) {
+        return res.status(400).send({ error: 'Product Not Found' });
+      }
+      return res.status(200).send(data);
+    })
+    .catch(next);
+});
 
 // ------- Delete Product Route -------
 server.delete('/:id', (req, res, next) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	Product.destroy({
-		where: { id }
-	})
-		.then(() => {
-			return res.status(200).send('Product Deleted');
-		})
-		.catch(next);
-})
- 
+  Product.destroy({
+    where: { id }
+  })
+    .then(() => {
+      return res.status(200).send('Product Deleted');
+    })
+    .catch(next);
+});
+
 // ------- Add Product to Category Route -------
 
 server.post('/:idProducto/category/:idCategoria', (req, res) => {
-	const idProducto = req.params.idProducto;
-	const idCategoria = req.params.idCategoria;
+  const idProducto = req.params.idProducto;
+  const idCategoria = req.params.idCategoria;
 
-	var product;
-	Product.findByPk(idProducto)
-		.then((data) => {
-			product = data;
-			return Category.findByPk(idCategoria)
-		})
-		.then((category) => {
-			product.addCategories(category)
-			res.status(200).send('Category added to Product');
-		})
-		.catch(err => {
-			res.send(err)
-		})
-})
+  var product;
+  Product.findByPk(idProducto)
+    .then((data) => {
+      product = data;
+      return Category.findByPk(idCategoria);
+    })
+    .then((category) => {
+      product.addCategories(category);
+      res.status(200).send('Category added to Product');
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 // ------- Delete Category from Product Route -------
 
 server.delete('/:idProducto/category/:idCategoria', (req, res) => {
-	const idProducto = req.params.idProducto;
-	const idCategoria = req.params.idCategoria;
+  const idProducto = req.params.idProducto;
+  const idCategoria = req.params.idCategoria;
 
-	var product;
-	Product.findByPk(idProducto)
-		.then((data) => {
-			product = data;
-			return Category.findByPk(idCategoria)
-		})
-		.then((category) => {
-			product.removeCategories(category)
-			res.status(200).send('Category deleted of the Product');
-		})
-		.catch(err => {
-			res.send(err)
-		})
-})
+  var product;
+  Product.findByPk(idProducto)
+    .then((data) => {
+      product = data;
+      return Category.findByPk(idCategoria);
+    })
+    .then((category) => {
+      product.removeCategories(category);
+      res.status(200).send('Category deleted of the Product');
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 // ------- Add Category Route -------
 server.post('/category/', (req, res, next) => {
-	const { name,description } = req.body;
+  const { name, description } = req.body;
 
-	Category.create({
-      name: name,
-      description:description    
-	})
-	.then(()=>{
-		res.send('Category created')
-	})
-	.catch(next)
-})
+  Category.create({
+    name: name,
+    description: description
+  })
+    .then(() => {
+      res.send('Category created');
+    })
+    .catch(next);
+});
 
 // ------- Delete Category Route -------
 server.delete('/category/:id', (req, res) => {
-	const { id } = req.params;
-	Category.destroy({
-		where: { id }
-	})
-		.then((data) => {
-			console.log(data)
-			if (!data) {
-				res.send('Invalid Category sent')
-			} else {
-				res.send('Category Deleted')
-			}
-		})
-		.catch(err => {
-			res.send('Error:', err)
-		})
+  const { id } = req.params;
+  Category.destroy({
+    where: { id }
+  })
+    .then((data) => {
+      console.log(data);
+      if (!data) {
+        res.send('Invalid Category sent');
+      } else {
+        res.send('Category Deleted');
+      }
+    })
+    .catch(err => {
+      res.send('Error:', err);
+    });
 });
 
 // ------- Update Category Route -------
 server.put('/category/:id', (req, res) => {
-	const { id } = req.params;
-	const { name, description } = req.body;
-	Category.update({
-		name,
-		description
-	}, {
-		where: { id }
-	})
-		.then((data) => {
-			console.log(data)
-			if (!data[0]) {
-				res.send('Invalid Category')
-			} else {
-				return res.status(200).send('Updated')
+  const { id } = req.params;
+  const { name, description } = req.body;
+  Category.update({
+    name,
+    description
+  }, {
+    where: { id }
+  })
+    .then((data) => {
+      console.log(data);
+      if (!data[0]) {
+        res.send('Invalid Category');
+      } else {
+        return res.status(200).send('Updated');
 
-			}
-		})
-		.catch(err => {
-			res.send('Error:', err)
-		})
-})
+      }
+    })
+    .catch(err => {
+      res.send('Error:', err);
+    });
+});
 
+// ------- Products X Category Route -------
+server.get("/category/:categoryName", (req, res, next) => {
+  const { categoryName } = req.params;
+
+  Product.findAll({
+    include: {
+      model: Category,
+      where: {
+        name: categoryName
+      }
+    },
+  })
+    .then((data) => {
+      // console.log(data[0].dataValues.categories[0].dataValues);
+      res.json(data);
+    })
+    .catch(next);
+});
 
 module.exports = server;
