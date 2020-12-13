e9577f13b82ad902c5b5ae5edc2bd5d37460b1de
gonzaloaguilarm <gonzaloaguilarm@gmail.com>
2020-12-13 03:15:29 -0300

    Finish CRUD Functionality and extras

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,157 +1,34 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Components
-import Product from '../components/Product';
 import SearchBar from '../components/SearchBar';
-import ProductForm from '../components/ProductForm';
-import Catalogue from '../components/Catalogue';
+import NavBar from '../components/NavBar/NavBar'
 
 // Styles
 import '../routes/App.css';
-
-const products = [{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-},
-{
-  title: "Geforce RTX 3080",
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  price: 799.99,
-  amount: 300,
-  images: [
-    "https://asset.msi.com/resize/image/global/product/product_7_20200917182157_5f6338c5cd72a.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png",
-    "https://www.venex.com.ar/products_images/1601906116_product_10_20200902111548_5f4f0e648ab77.png",
-    "https://imagenes.coolmod.com/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x-tarjeta-grafica-005.jpg"
-  ]
-}];
-
-const category = [{
-  name: "CPU"
-},
-{
-  name: "CPU Cooler"
-},
-{
-  name: "Motherboard"
-},
-{
-  name: "Memory"
-},
-{
-  name: "Storage"
-},
-{
-  name: "Video Card"
-},
-{
-  name: "Power Supply"
-},
-{
-  name: "Case"
-}];
-
-const reviews = {
-  average: 4.4,
-  total: 40,
-};
+import Carousel from '../components/Carousel/Carousel';
 
 
 const Home = () => {
   return (
     <div className="App">
       <div style={{ margin: "0 10vw" }}>
-        <SearchBar handleSubmit={(e) => {
-          e.preventDefault();
-          console.log("Enviado");
-        }} />
-        <Link to="/admin">
-          <button>Admin</button>
-        </Link>
-        <Switch>
-          <Route path="/products">
-            <Catalogue product={products} category={category} reviews={reviews} />
-          </Route>
-          <Route path="/product/:id">
-            <Product product={products[0]} category={category[5]} reviews={reviews} />
-          </Route>
-          <Route exact path="/admin">
-            <ProductForm>Nuevo Producto</ProductForm>
-          </Route>
-        </Switch>
+      <NavBar>
+        <Link to="/">Home</Link>
+        <Link to="/">Builder</Link>
+        <Link to="/">PcBuilts</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/">About Us</Link>
+          <SearchBar handleSubmit={(e) => {
+            e.preventDefault();
+            console.log("Enviado");
+          }} />
+        </NavBar>
+        <Carousel/>
+        
+        
+        
       </div>
     </div>
   );
--- client/src/containers/Layout.jsx
+++ client/src/containers/Layout.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
 const Layout = ({ children }) => (
-  <div>
+  <div className='container'>
     <Header />
     {children}
     <Footer />
