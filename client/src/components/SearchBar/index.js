ad7f6ca36f0141288d5ac7f0cff44e4410e644ea
Chusto <38694567+Chusto@users.noreply.github.com>
2020-12-13 18:35:41 -0300

    SearchBar tweak

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 
+// Components
+import Button from "../Button";
+
 // Styled Components
 import { SearchBarForm, SearchInput, InputWrapper, SearchIconStyled } from "./styles";
 
 const SearchBar = ({ handleSubmit }) => {
-
   return (
     <SearchBarForm onSubmit={handleSubmit}>
 
       <InputWrapper>
+        <SearchIconStyled />
         <SearchInput placeholder={"Search..."} />
       </InputWrapper>
 
-      <SearchIconStyled />
+      <Button>Search</Button>
+
     </SearchBarForm>
   );
 };
