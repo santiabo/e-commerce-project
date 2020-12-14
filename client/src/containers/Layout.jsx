05c29322efbbbe12c82d95cb71722f053444622c
Chusto <38694567+Chusto@users.noreply.github.com>
2020-12-14 15:49:54 -0300

    Styled-Footer v1

--- client/src/containers/Layout.jsx
+++ client/src/containers/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '../components/Header/index';
-import Footer from '../components/Footer/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Layout = ({ children }) => (
   <>
