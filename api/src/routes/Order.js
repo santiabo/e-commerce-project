33cc3fef86db127fca3936d693bf30704b61a5f4
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2020-12-17 11:32:12 -0300

    Progreso en rutas de Ordenes

--- api/src/routes/Order.js
+++ api/src/routes/Order.js
@@ -28,3 +28,45 @@ server.post('users/:idUser/cart', (req, res, next) => {
 });
 
 // FALTA TERMINAR.
+
+// ---> 44 45 46 47 <-- falta terminar de armar dos get y el put
+
+server.get('/status/:status', (req, res) => { 
+  //Esta ruta puede recibir el query string "status" y deberá devolver sólo las ordenes con ese status.
+//vamos a adivinar
+  var status = req.params.status //query string status
+  Order.findAll({ //busca todas las ordenes
+    where: {
+      status //que tengan este argumento especifico (un estado)
+    }
+  }).then((orders)=>{
+    return res.send(orders) //devuelve esas ordenes
+  }).catch((err)=>{
+    return res.send(err) //o devuelve un error
+  })
+
+ });
+
+ server.get('/users/:id/orders', (req, res, next) => { 
+  //devuelve las ordenes de usuarios
+ 
+
+
+ });
+
+ 
+
+ server.put('/edit/id/:id', (req, res) => {
+
+  //modifica una orden
+  
+  
+  });
+
+  // server.get('/:id', (req, res) => { 
+  //   //una orden particular
+    
+  //  });
+
+
+ module.exports = server;
