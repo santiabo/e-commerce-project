e6901563bc51622fabcfacece93b33726a32057d
santiabo <santiabo@gmail.com>
2020-12-17 09:12:34 -0300

    Fixed items amount changes

--- api/src/routes/Order.js
+++ api/src/routes/Order.js
@@ -32,81 +32,84 @@ server.post('/users/:userId/cart', (req, res, next) => {
         })
         .catch(next);
       // Si ya tiene una order.
-      /*  } else {
-           // Tiene una orderLine con ese producto ?
-           OrderLine.findOne({
-             where: {
-               productId: idProduct,
-               orderId: order.id
-             }
-           }).then((orderLine) => {
-             
-             //Si ya tiene orderLine con ese producto, se le suma la cantidad.
-             if(orderLine){
-               
-               OrderLine.update (
-                 {quantity: amount},
-                 {where: {productId: idProduct}}
-                 )
-                 .then((data) => {
-                   res.send({ ...data.dataValues });
-                 })
-                 .catch(next); */
     } else {
+      // Tiene una orderLine con ese producto ?
+      OrderLine.findOne({
+        where: {
+          productId: idProduct,
+          orderId: order.id
+        }
+      }).then((orderLine) => {
+        console.log(orderLine.dataValues)
+        //Si ya tiene orderLine con ese producto, se le suma la cantidad.
+        if (orderLine) {
+          console.log(orderLine.dataValues)
+          OrderLine.update(
+            { quantity: amount },
+            { where: { productId: idProduct } }
+          )
+            .then((orderLine) => {
+             return  res.send({ItemsQuantity: `Changed to: ${amount}`});
+            })
+            .catch(next);
+        } else {
 
-      //si no tiene, se crea una OrderLine
-      OrderLine.create({
-        quantity: amount,
-        productId: idProduct,  // Le asigno el id del producto.
-        orderId: order.id   // Le asigno el id de la orden
-      })
-        .then((orderLine) => {
-          return res.send({ ...orderLine.dataValues });
-        })
-        .catch(next);
-
-    }
-
-  })
-
-})
-
-server.get('/users/:userId/cart', (req, res, next) => {
-  const { userId } = req.params;
-
-  Order.findOne({
-    where: {
-      client_id: userId,
-      status: 'on_cart'
-    }
-  })
-    .then((order) => {
-      if (!order) {
-        return res.status(404).send({ error: `User doesn't have an order` })
-      } else
-        OrderLine.findAll({
-          where: { orderId: order.id }
-        })
-          .then((orderLine) => {
-            let totalProducts = orderLine.map(e => `Id: ${e.productId} Amount: ${e.quantity}`);
-            return res.send({ totalProducts })
+          //si no tiene, se crea una OrderLine
+          OrderLine.create({
+            quantity: amount,
+            productId: idProduct,  // Le asigno el id del producto.
+            orderId: order.id   // Le asigno el id de la orden
           })
-    })
-    .catch(next)
+            .then((orderLine) => {
+              return res.send({ ...orderLine.dataValues });
+            })
+            .catch(next);
 
-})
+        }
 
-server.delete('/users/:userId/cart', (req, res, next) => {
-  const { userId } = req.params;
+      })
 
-  Order.destroy({
-    where: { client_id: userId }
+    }
   })
-    .then((data) => {
-      return res.send({ CartDeletd: Number(userId) });
-
-    })
-    .catch(next);
 });
 
-module.exports = server;
+
+  server.get('/users/:userId/cart', (req, res, next) => {
+    const { userId } = req.params;
+
+    Order.findOne({
+      where: {
+        client_id: userId,
+        status: 'on_cart'
+      }
+    })
+      .then((order) => {
+        if (!order) {
+          return res.status(404).send({ error: `User doesn't have an order` })
+        } else
+          OrderLine.findAll({
+            where: { orderId: order.id }
+          })
+            .then((orderLine) => {
+              let totalProducts = orderLine.map(e => `Id: ${e.productId} Amount: ${e.quantity}`);
+              return res.send({ totalProducts })
+            })
+      })
+      .catch(next)
+
+  })
+
+  server.delete('/users/:userId/cart', (req, res, next) => {
+    const { userId } = req.params;
+
+    Order.destroy({
+      where: { client_id: userId }
+    })
+      .then((data) => {
+        return res.send({ CartDeletd: Number(userId) });
+
+      })
+      .catch(next);
+  });
+
+  module.exports = server;
