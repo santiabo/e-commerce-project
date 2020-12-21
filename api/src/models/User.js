5a06c12efd4f9868548bb9d348b5d87e3ed2fc1e
Chusto <38694567+Chusto@users.noreply.github.com>
2020-12-21 19:11:19 -0300

    Add password to User model and log in route.

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -8,37 +8,52 @@ module.exports = (sequelize) => {
       allowNull: false,
       unique: true,
     },
+
     /*  isAadmin: {
        type: DataTypes.STRING,     <-- Se va a usar después!
        allowNull: false,
      }, */
+
     avatar: {
       type: DataTypes.TEXT,
       isUrl: true  //   <--- Valida que sea URL.
-
     },
+
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
       isAlpha: true, //    <--- Sólo letras
       len: [2, 40]
     },
+
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
       isAlpha: true,
       len: [2, 40]
     },
+
     birthdate: {
       type: DataTypes.DATEONLY,
       allowNull: false,
       isDate: true//  <--- Sólo fechas
     },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       len: [6, 12],    // Más de 6 caracteres y con una mayuscula y un número al menos.
-      is: ["^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"]
+      is: ["^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"],
+      // get() {
+      //   return () => this.getDataValue('password');
+      // },
     },
+    
+    salt: {
+      type: DataTypes.STRING,
+      // get() {
+      //   return () => this.getDataValue('salt');
+      // }
+    }
   });
 };
