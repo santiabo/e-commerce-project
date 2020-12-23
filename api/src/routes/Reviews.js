3d7cf122351ac6963dd252e83292e9f2dfe72022
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2020-12-23 12:31:00 -0300

    progress on rev list

--- api/src/routes/Reviews.js
+++ api/src/routes/Reviews.js
@@ -1,5 +1,18 @@
-const server = require('express').Router();
+const Reviews = require('../models/Reviews');
 
+const server = require('express').Router();
+const {Product, Review} = require('../db.js');
+
+server.get('/product/:id/review', (req, res, next) => {
+  //devuelve las ordenes de usuarios
+  const { id } = req.params;
+
+  Review.findAll({ //busca las reviews
+    where: { id } //<-- del producto especifico
+  }).then((review) => {
+    return res.send(review); //devuelve las reviews
+  }).catch(next);
+});
 
 
 
