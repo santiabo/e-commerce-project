237379a88cf718d4faf5a3c7b7edf2bd887518d4
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2020-12-23 14:55:56 -0300

    reviews of a product

--- api/src/routes/Reviews.js
+++ api/src/routes/Reviews.js
@@ -1,10 +1,10 @@
 const Reviews = require('../models/Reviews');
 
 const server = require('express').Router();
-const {Product, Review} = require('../db.js');
+const { Review } = require('../db.js');
 
 server.get('/product/:id/review', (req, res, next) => {
-  //devuelve las ordenes de usuarios
+  //devuelve las reviews del producto
   const { id } = req.params;
 
   Review.findAll({ //busca las reviews
