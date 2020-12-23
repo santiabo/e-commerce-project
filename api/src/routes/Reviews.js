58e8dd364ed000548b5f1938536ec4c7a3168f68
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2020-12-23 15:18:37 -0300

    delete

--- api/src/routes/Reviews.js
+++ api/src/routes/Reviews.js
@@ -1,5 +1,3 @@
-const Reviews = require('../models/Reviews');
-
 const server = require('express').Router();
 const { Review } = require('../db.js');
 
@@ -14,6 +12,19 @@ server.get('/product/:id/review', (req, res, next) => {
   }).catch(next);
 });
 
+server.delete('/product/:id/review/:idReview', (req, res, next) => {
+  //elimina review
+  const { id } = req.params;
+
+  Review.destroy({ //destruye
+    where: { id } //determinada review
+  })
+    .then((data) => { //una vez destruida
+      if (data) return res.send({ reviewDeleted: Number(id) }); //confirma
+      return res.status(404).send({ error: "Review not Found." }); //o envia un error si no la encuentra
+    })
+    .catch(next);
+});
 
 
 module.exports = server;
