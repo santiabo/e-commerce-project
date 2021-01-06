d9484804a88c2e1ef81409e15ef46d1ae7bc8294
Chusto <38694567+Chusto@users.noreply.github.com>
2021-01-06 01:31:04 -0300

    UserLoggedIn component, moved RegisterForm styles to a .css file

--- client/src/components/Footer/styles.jsx
+++ client/src/components/Footer/styles.jsx
@@ -19,15 +19,19 @@ export const SocialMediaWrapper = styled.div`
   justify-content: center;
   
   svg {
-  transition: fill 0.45s cubic-bezier(0.165, 0.84, 0.44, 1);
-    width: 24px;
-    height: 24px;
+  transition: 0.45s cubic-bezier(0.165, 0.84, 0.44, 1);
+  width: 24px;
+  height: 24px;
   margin: 1.5rem;
   fill: #ffffff;
   cursor: pointer;
+  opacity: 0.7;
 
   &:hover {
     transform: translateY(-4px);
+    opacity: 1;
+    width: 26px;
+    height: 26px;
     }
   }
 `;
