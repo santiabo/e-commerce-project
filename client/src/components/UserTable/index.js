e17579619e09df6554d9406fa56a906e9ccb801a
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-01-06 11:50:50 -0300

    bosquejo usertable

--- client/src/components/UserTable/index.js
+++ client/src/components/UserTable/index.js
@@ -20,6 +20,8 @@ function userTable(){
        <th>First Name</th>
        <th>Last Name</th>
        <th>Email</th>
+       <th>Role</th>
+       <th>Settings</th>
      </tr>
    </thead>
    <tbody>
@@ -28,17 +30,23 @@ function userTable(){
        <td>Mark</td>
        <td>Otto</td>
        <td>@mdo</td>
+       <td>User</td>
+       <td><button>Edit Role</button></td>
      </tr>
      <tr>
        <td>2</td>
        <td>Jacob</td>
        <td>Thornton</td>
        <td>@fat</td>
+       <td>Admin</td>
+       <td><button>Edit Role</button></td>
      </tr>
      <tr>
        <td>3</td>
        <td colSpan="2">Larry the Bird</td>
        <td>@twitter</td>
+       <td>User</td>
+       <td><button>Edit Role</button></td>
      </tr>
    </tbody>
  </table>
