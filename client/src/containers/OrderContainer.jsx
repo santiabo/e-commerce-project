3739ae80857cb995e9971dc124ef9a315445b21a
santiabo <santiabo@gmail.com>
2021-01-10 08:55:03 -0300

    User order list

--- client/src/containers/OrderContainer.jsx
+++ client/src/containers/OrderContainer.jsx
@@ -2,15 +2,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import Order from '../components/Order/Order'
 import { getUserOrders } from '../redux/actions/order';
 
 
 const OrderContainer = () => {
   const dispatch = useDispatch();
-  const history = useHistory(); 
-  const  user = useSelector(state => state.user)
-  const  isUser = useSelector(state => state.user.isUser)
+  const history = useHistory();
+  const user = useSelector(state => state.user)
+  const isUser = useSelector(state => state.user.isUser)
   console.log("USER ID >>>>>>> ", user.user.id)
   useEffect(() => {
     if (!isUser) {
@@ -19,44 +18,53 @@ const OrderContainer = () => {
   }, [])
 
   useEffect(() => {
-   dispatch(getUserOrders(user.user.id))
+    dispatch(getUserOrders(user.user.id))
   }, [])
 
   const userOrders = useSelector(state => state.order.userOrders);
-  console.log("USER ORDERS >>>>> ", userOrders)
 
-  
-  const getTotal = (userOrders) => {
-    return Number(userOrders.reduce((acc, orderLine)=> {
-      acc = orderLine.reduce((acc, el)=> {
-        acc = el.quantity * el.price;
-      })
-    }))};
- 
+  const getTotal = (obj) => {
+    let suma = 0;
+    console.log(obj[0])
+    for (let j = 0; j < obj.orderLines.length; j++) {
+      suma = suma + (obj.orderLines[j].price * obj.orderLines[j].quantity)
+    }
+
+    return suma;
+  };
+
   return (
-    <div>
-      <h1>User Orders</h1>
-      {userOrders.map((i) =>
-      <>        
-
-          <h2>ID: {i.id}</h2>
-          <h2>Status: {i.status}</h2>
-          <h2>{getTotal(userOrders)}</h2>
-          {i.orderLines.map((e) =>
-          
-            <Order
-              img={e.product.images[0]}
-              name={e.product.name}
-              price={e.product.price}
-              quantity={e.quantity}
-              totalPrice={e.price} />
-          )}
-        </>
-      )}
-
+    <div className="App">
+      <h2>Orders</h2>
+      <section>
+        <table width="100%" border="1" bordercolor="#000000" cellspacing="10" cellpadding="10" >
+          <thead>
+            <tr valign="middle" align="center">
+              <th>id </th>
+              <th>Creation date</th>
+              <th>Last update</th>
+              <th>Total</th>
+              <th>Status</th>
+              <th>  </th>
+            </tr>
+          </thead>
+          <tbody>
+            {userOrders !== undefined &&
+              userOrders.map((i) =>
+                <tr valign="middle" align="center">
+                  <td>{i.id}</td>
+                  <td>{i.createdAt.slice(0, 10)}</td>
+                  <td>{i.updatedAt.slice(0, 10)}</td>
+                  <td>$ {getTotal(i)}</td>
+                  <td>{i.status}</td>
+                  <img src={i.orderLines[0].product.images[0]} width="75" height="75"></img>
+                </tr>
+              )}
+          </tbody>
+        </table>
+      </section>
     </div>
-  );
+  )
 };
-
 
 export default OrderContainer;
