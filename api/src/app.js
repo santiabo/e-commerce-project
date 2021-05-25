d5eda0869cdd4680dd823181e2ffab23df8ef188
Santiago Aguirre <santiabo@gmail.com>
2021-05-25 11:49:38 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-//HOLA !
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
